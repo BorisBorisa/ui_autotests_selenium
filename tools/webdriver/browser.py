5a9fedcb549c2775f41387c6dc33792b0b60c4d0
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-20 02:45:36 +0300

    add browser factory

---
+++ tools/webdriver/browser.py
@@ -0,0 +1,72 @@
+from typing import Type
+
+from selenium.webdriver import (
+    Chrome, ChromeOptions,
+    Firefox, FirefoxOptions,
+    Edge, EdgeOptions
+)
+
+from selenium.webdriver.remote.webdriver import WebDriver
+
+from config import settings, Browser
+
+
+class BaseBrowser:
+    def __init__(self, driver_class, options_class, browser_config):
+        self.driver_class: Type[WebDriver] = driver_class
+        self.options_class = options_class
+        self.browser_config = browser_config
+
+    def create_driver(self) -> WebDriver:
+        # Добавить логирование
+
+        options = self.options_class()
+
+        for arg in self.browser_config.options:
+            options.add_argument(arg)
+
+        options.page_load_strategy = settings.browsers_config.page_load_strategy
+
+        driver = self.driver_class(options=options)
+        driver.set_page_load_timeout(settings.browsers_config.page_load_timeout)
+
+        return driver
+
+
+class ChromeBrowser(BaseBrowser):
+    def __init__(self):
+        super().__init__(
+            driver_class=Chrome,
+            options_class=ChromeOptions,
+            browser_config=settings.browsers_config.chromium_config
+        )
+
+
+class FirefoxBrowser(BaseBrowser):
+    def __init__(self):
+        super().__init__(
+            driver_class=Firefox,
+            options_class=FirefoxOptions,
+            browser_config=settings.browsers_config.firefox_config
+        )
+
+
+class EdgeBrowser(BaseBrowser):
+    def __init__(self):
+        super().__init__(
+            driver_class=Edge,
+            options_class=EdgeOptions,
+            browser_config=settings.browsers_config.chromium_config
+        )
+
+
+def get_browser_driver(browser_name: str) -> WebDriver:
+    match browser_name:
+        case Browser.CHROME:
+            return ChromeBrowser().create_driver()
+        case Browser.FIREFOX:
+            return FirefoxBrowser().create_driver()
+        case Browser.EDGE:
+            return EdgeBrowser().create_driver()
+        case _:
+            raise ValueError(f"Неизвестный браузер: {browser_name}")
