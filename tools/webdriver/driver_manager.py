545c6d33c7697f0afcc554b15cb1388b193bde15
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-20 13:52:54 +0300

    refactor replace driver property with get_driver method

--- tools/webdriver/driver_manager.py
+++ tools/webdriver/driver_manager.py
@@ -13,8 +13,7 @@ class DriverManger(metaclass=Singleton):
     def init(self, browser: Browser):
         self._driver = get_browser_driver(browser)
 
-    @property
-    def driver(self) -> WebDriver:
+    def get_driver(self) -> WebDriver:
         if self._driver is None:
             raise RuntimeError("Driver not initialized. Call init(browser) first.")
         return self._driver
