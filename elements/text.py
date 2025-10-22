c5eb4150732beba35f6f916228e091d9ad90da81
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-22 06:24:23 +0300

    add get_inner_text method

--- elements/text.py
+++ elements/text.py
@@ -2,6 +2,8 @@ from elements.base_element import BaseElement
 
 from selenium.webdriver.common.by import ByType
 
+from tools.webdriver.waiter import Waiter
+
 
 class Text(BaseElement):
     def __init__(self, name: str, locator_type: ByType, locator: str):
@@ -9,3 +11,8 @@ class Text(BaseElement):
 
     def type_of(self) -> str:
         return "text"
+
+    def get_inner_text(self, **kwargs) -> str:
+        locator = self.get_locator(**kwargs)
+        element = Waiter.visible(locator)
+        return element.text
