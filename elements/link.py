790a2d4667a5a3aa01ee03558f478d7d81faf86e
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-25 04:38:03 +0300

    refactor remove __init__

--- elements/link.py
+++ elements/link.py
@@ -1,11 +1,6 @@
 from elements.base_element import BaseElement
 
-from selenium.webdriver.common.by import ByType
-
 
 class Link(BaseElement):
-    def __init__(self, name: str, locator_type: ByType, locator: str):
-        super().__init__(name, locator_type, locator)
-
     def type_of(self) -> str:
         return "link"
--- elements/text.py
+++ elements/text.py
@@ -1,14 +1,9 @@
 from elements.base_element import BaseElement
 
-from selenium.webdriver.common.by import ByType
-
 from tools.webdriver.waiter import Waiter
 
 
 class Text(BaseElement):
-    def __init__(self, name: str, locator_type: ByType, locator: str):
-        super().__init__(name, locator_type, locator)
-
     def type_of(self) -> str:
         return "text"
 
