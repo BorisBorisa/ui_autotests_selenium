cc18985deb7980ffb1638c212373a782eb735d27
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-25 04:43:33 +0300

    add tabel column header enum

---
+++ tools/enums/column_header_enum.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class ColumnHeader(Enum):
+    FIRST_NAME = "First Name"
+    LAST_NAME = "Last Name"
+    AGE = "Age"
+    EMAIL = "Email"
+    SALARY = "Salary"
+    DEPARTMENT = "Department"
+    ACTION = "Action"
