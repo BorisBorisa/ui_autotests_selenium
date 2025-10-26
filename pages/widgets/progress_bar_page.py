2d59df0fb28df7fcad46221f75143dfec1a5bc51
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-26 13:56:50 +0300

    add progress bar page

---
+++ pages/widgets/progress_bar_page.py
@@ -0,0 +1,34 @@
+from selenium.webdriver.common.by import By
+
+from pages.base_page import BasePage
+
+from components.progress_bar_component import ProgressBarComponent
+from components.navigation.header_component import HeaderComponent
+from components.navigation.left_panel_component import LeftPanelComponent
+
+from elements.text import Text
+from elements.button import Button
+
+
+class ProgressBarPage(BasePage):
+    def __init__(self):
+        super().__init__()
+
+        self.header = HeaderComponent()
+        self.left_panel = LeftPanelComponent()
+
+        self.title = Text("title", By.XPATH, '//*[@id="progressBarContainer"]/h1')
+        self.start_stop_button = Button("start stop", By.ID, "startStopButton")
+
+        self.progress_bar = ProgressBarComponent()
+
+    def is_page_opened(self):
+        self.title.check_visible()
+        self.title.check_have_text("Progress Bar")
+
+    def check_bar_value_matches_expected(self, expected_value: int):
+        actual_value = self.progress_bar.get_value()
+
+        assert actual_value == expected_value, (
+            f'Bar value mismatch: expected "{expected_value}", got "{actual_value}"'
+        )
