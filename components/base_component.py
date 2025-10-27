a50103e6c3dd9f5458f2c1285f70605bc3564b57
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-27 18:28:59 +0300

    add base component

---
+++ components/base_component.py
@@ -0,0 +1,15 @@
+import allure
+
+from tools.webdriver.waiter import Waiter
+
+
+class BaseComponent:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def check_current_url(expected_url: str):
+        step = f"Check that current URL matches expected: '{expected_url}'"
+
+        with allure.step(step):
+            Waiter.url_matches(expected_url)
