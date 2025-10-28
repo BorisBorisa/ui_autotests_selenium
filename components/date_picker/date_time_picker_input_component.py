419819e160d318b399257fe715596ec2165786d8
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-28 15:53:37 +0300

    add allure steps

--- components/date_picker/date_time_picker_input_component.py
+++ components/date_picker/date_time_picker_input_component.py
@@ -1,3 +1,5 @@
+import allure
+
 from datetime import datetime
 
 from selenium.webdriver.common.by import By
@@ -9,20 +11,23 @@ class DateTimePickerComponent:
     def __init__(self):
         self.date_picker_input = Input("date picker", By.ID, 'dateAndTimePickerInput')
 
-    def open_calendar(self):
+    def click_date_picker_input(self):
         self.date_picker_input.click()
 
+    @allure.step("Set datetime via input: {date_str}")
     def set_data_by_input(self, date_str: str):
         self.date_picker_input.clear()
         self.date_picker_input.fill(date_str)
 
-    def get_selected_date(self) -> datetime:
+    @allure.step("Get selected datetime from input")
+    def get_selected_datetime(self) -> datetime:
         value = self.date_picker_input.get_attr("value")
         return datetime.strptime(value, "%B %d, %Y %I:%M %p")
 
-    def check_current_date_matches_expected(self, expected_date: datetime):
-        actual_date = self.get_selected_date()
-        expected_date = expected_date.replace(second=0, microsecond=0)
-        assert actual_date == expected_date, (
-            f'Selected date {actual_date} does not match expected date {expected_date}'
+    @allure.step("Check that selected datetime equals expected datetime {expected_datetime:%d %B %Y %H:%M}")
+    def check_current_date_matches_expected(self, expected_datetime: datetime):
+        actual_datetime = self.get_selected_datetime()
+        expected_datetime = expected_datetime.replace(second=0, microsecond=0)
+        assert actual_datetime == expected_datetime, (
+            f'Selected date {actual_datetime} does not match expected date {expected_datetime}'
         )
