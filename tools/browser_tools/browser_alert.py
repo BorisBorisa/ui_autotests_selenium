cf29cc488baff59f0bb98f8c6b0fcda9e786fc6a
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-29 20:06:35 +0300

    add assert message to check_alert_text

--- tools/browser_tools/browser_alert.py
+++ tools/browser_tools/browser_alert.py
@@ -28,5 +28,6 @@ class BrowserAlert:
     def get_alert_text() -> str:
         return Waiter().alert_is_present().text
 
-    def check_alert_text(self, text: str):
-        assert self.get_alert_text() == text, "!!!!!!"
+    def check_alert_text(self, expected_text: str):
+        actual_text = self.get_alert_text()
+        assert actual_text == expected_text, f"Expected alert text '{expected_text}', but got '{actual_text}'"
